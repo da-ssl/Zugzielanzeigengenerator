4ca025e335697586942c4d62bee52c54977721ce
da-ssl <tassilo@thewalt.de>
2022-04-18 13:18:51 +0200

    Lauftext + Umstrukturierung des Codes.

--- js/main.js
+++ js/main.js
@@ -60,7 +60,7 @@ function Eingabe() {
 
     var c = document.getElementById("text01");
 
-    //Ziel des Zuges
+    //Zug 1
 
     var ziel1 = c.getContext("2d");
     ziel1.font = "140px lcdzza10px"
@@ -68,44 +68,10 @@ function Eingabe() {
     ziel1.textAlign = "left"; 
     ziel1.fillText(zug1ziel, 772, 512)
 
-    var ziel2 = c.getContext("2d");
-    ziel2.font = "140px lcdzza10px"
-    ziel2.fillStyle ="white";
-    ziel2.fillText(zug2ziel, 772, 672)
-
-    var ziel3 = c.getContext("2d");
-    ziel3.font = "140px lcdzza10px"
-    ziel3.fillStyle ="white";
-    ziel3.fillText(zug3ziel, 772, 832)
-
-    var ziel4 = c.getContext("2d");
-    ziel4.font = "140px lcdzza10px"
-    ziel4.fillStyle ="white";
-    ziel4.fillText(zug4ziel, 772, 1000)
-
-    //S-Bahn-Linie
-
     var linie1 =c.getContext("2d");
     linie1.font= "140px lcdzza10px-linien"
     linie1.fillStyle = "white";
     linie1.fillText(zug1linie, 500, 512)
-
-    var linie2 =c.getContext("2d");
-    linie2.font= "140px lcdzza10px-linien"
-    linie2.fillStyle = "white";
-    linie2.fillText(zug2linie, 500, 672)
-
-    var linie3 =c.getContext("2d");
-    linie3.font= "140px lcdzza10px-linien"
-    linie3.fillStyle = "white";
-    linie3.fillText(zug3linie, 500, 832)
-
-    var linie4 = c.getContext("2d");
-    linie4.font = "140px lcdzza10px-linien"
-    linie4.fillstyle = "white";
-    linie4.fillText(zug4linie, 500, 1000)
-
-    //Gleis
 
     var gleis1 = c.getContext("2d");
     gleis1.font = "140px lcdzza10px"
@@ -113,51 +79,11 @@ function Eingabe() {
     gleis1.textAlign = "end"; 
     gleis1.fillText(zug1gleis, 2470, 512);
 
-    var gleis2 = c.getContext("2d");
-    gleis2.font = "140px lcdzza10px"
-    gleis2.fillStyle = "white";
-    gleis2.textAlign = "end"; 
-    gleis2.fillText(zug2gleis, 2470, 672);
-
-    var gleis3 = c.getContext("2d");
-    gleis3.font = "140px lcdzza10px"
-    gleis3.fillStyle = "white";
-    gleis3.textAlign = "end"; 
-    gleis3.fillText(zug3gleis, 2470, 832);
-
-    var gleis4 = c.getContext("2d");
-    gleis4.font = "140px lcdzza10px"
-    gleis4.fillStyle = "white";
-    gleis4.textAlign = "end"; 
-    gleis4.fillText(zug4gleis, 2470, 1000);
-
-    //Wagenstandsanzeiger
-
     var ws1 = c.getContext("2d");
     ws1.font = "140px lcdzza10px"
     ws1.fillStyle = "white";
     ws1.textAlign = "center"; 
     ws1.fillText(zug1ws, 2790, 512);
-
-    var ws2 = c.getContext("2d");
-    ws2.font = "140px lcdzza10px"
-    ws2.fillStyle = "white";
-    ws2.textAlign = "center"; 
-    ws2.fillText(zug2ws, 2790, 672);
-
-    var ws3 = c.getContext("2d");
-    ws3.font = "140px lcdzza10px"
-    ws3.fillStyle = "white";
-    ws3.textAlign = "center"; 
-    ws3.fillText(zug3ws, 2790, 832);
-
-    var ws4 = c.getContext("2d");
-    ws4.font = "140px lcdzza10px"
-    ws4.fillStyle = "white";
-    ws4.textAlign = "center"; 
-    ws4.fillText(zug4ws, 2790, 1000);
-
-    //In Min
 
     var min1 = c.getContext("2d");
     min1.font = "140px lcdzza10px"
@@ -165,11 +91,61 @@ function Eingabe() {
     min1.textAlign = "end"; 
     min1.fillText(zug1min, 3480, 512);
 
+    //Zug 2
+
+    var ziel2 = c.getContext("2d");
+    ziel2.font = "140px lcdzza10px"
+    ziel2.fillStyle ="white";
+    ziel2.textAlign = "left"; 
+    ziel2.fillText(zug2ziel, 772, 672)
+
+    var linie2 =c.getContext("2d");
+    linie2.font= "140px lcdzza10px-linien"
+    linie2.fillStyle = "white";
+    linie2.fillText(zug2linie, 500, 672)
+
+    var gleis2 = c.getContext("2d");
+    gleis2.font = "140px lcdzza10px"
+    gleis2.fillStyle = "white";
+    gleis2.textAlign = "end"; 
+    gleis2.fillText(zug2gleis, 2470, 672);
+    
+    var ws2 = c.getContext("2d");
+    ws2.font = "140px lcdzza10px"
+    ws2.fillStyle = "white";
+    ws2.textAlign = "center"; 
+    ws2.fillText(zug2ws, 2790, 672);
+
     var min2 = c.getContext("2d");
     min2.font = "140px lcdzza10px"
     min2.fillStyle = "white";
     min2.textAlign = "end"; 
     min2.fillText(zug2min, 3480, 672);
+
+    //Zug 3
+
+    var ziel3 = c.getContext("2d");
+    ziel3.font = "140px lcdzza10px"
+    ziel3.fillStyle ="white";
+    ziel3.textAlign = "left"; 
+    ziel3.fillText(zug3ziel, 772, 832)
+
+    var linie3 =c.getContext("2d");
+    linie3.font= "140px lcdzza10px-linien"
+    linie3.fillStyle = "white";
+    linie3.fillText(zug3linie, 500, 832)
+
+    var gleis3 = c.getContext("2d");
+    gleis3.font = "140px lcdzza10px"
+    gleis3.fillStyle = "white";
+    gleis3.textAlign = "end"; 
+    gleis3.fillText(zug3gleis, 2470, 832);
+
+    var ws3 = c.getContext("2d");
+    ws3.font = "140px lcdzza10px"
+    ws3.fillStyle = "white";
+    ws3.textAlign = "center"; 
+    ws3.fillText(zug3ws, 2790, 832);
 
     var min3 = c.getContext("2d");
     min3.font = "140px lcdzza10px"
@@ -177,11 +153,87 @@ function Eingabe() {
     min3.textAlign = "end"; 
     min3.fillText(zug3min, 3480, 832);
 
-    var min4 = c.getContext("2d");
-    min4.font = "140px lcdzza10px"
-    min4.fillStyle = "white";
-    min4.textAlign = "end"; 
-    min4.fillText(zug4min, 3480, 1000);
+    //Zug 4
+
+    const iflt4 = document.querySelector('#lauftextletztezeile');
+
+    if(iflt4.checked == false){
+    
+        var ziel4 = c.getContext("2d");
+        ziel4.font = "140px lcdzza10px"
+        ziel4.fillStyle ="white";
+        ziel4.textAlign = "left"; 
+        ziel4.fillText(zug4ziel, 772, 1000)
+        
+        var linie4 = c.getContext("2d");
+        linie4.font = "140px lcdzza10px-linien"
+        linie4.fillstyle = "white";
+        linie4.fillText(zug4linie, 500, 1000)
+        
+        var gleis4 = c.getContext("2d");
+        gleis4.font = "140px lcdzza10px"
+        gleis4.fillStyle = "white";
+        gleis4.textAlign = "end"; 
+        gleis4.fillText(zug4gleis, 2470, 1000);
+        
+        var ws4 = c.getContext("2d");
+        ws4.font = "140px lcdzza10px"
+        ws4.fillStyle = "white";
+        ws4.textAlign = "center"; 
+        ws4.fillText(zug4ws, 2790, 1000);
+        
+        var min4 = c.getContext("2d");
+        min4.font = "140px lcdzza10px"
+        min4.fillStyle = "white";
+        min4.textAlign = "end"; 
+        min4.fillText(zug4min, 3480, 1000);
+    } else 
+    {
+        /*var lauftext4 = c.getContext("2d");
+        const move_pixel = 1;
+        var x = 1000;
+        lauftext4.font = "140px lcdzza10px";
+        lauftext4.fillStyle = "white";
+        var lauftext4text = "Hier steht ihr Lauftext, der nur als Beispiel dient.";
+        var text_width = lauftext4.measureText(lauftext4text).width;
+        lauftext4.fillText(lauftext4text, 3480, x);
+
+        function lauftext4beweg()
+        {
+            lauftext4.clearRect(3480,1000,x,2000);
+            if (x > text_width) {
+                x = x - move_pixel;
+            } else {
+                x = 500;
+            }
+            lauftext4.fillText(lauftext4text, x, 1000);
+            window.requestAnimationFrame(lauftext4beweg);
+        }*/
+
+        const move_pixel = 3;
+        var lauftext4 = c.getContext("2d");
+        lauftext4.font = "140px lcdzza10px";
+        var x = 3480;
+        var lauftext4text = document.getElementById("ibZug4lauftext").value;
+        var text_width = lauftext4.measureText(lauftext4text).width;
+        lauftext4.fillText(lauftext4text, x, 3480);
+
+        window.requestAnimationFrame(moveTicker);
+        function moveTicker()
+        {
+            lauftext4.clearRect(0,890,x,2000);
+            if (x > 480 /*-text_width*/)
+                x = x - move_pixel;
+            else
+                x = 4000 + text_width;
+            lauftext4.fillText(lauftext4text, x, 1000);	
+            window.requestAnimationFrame(moveTicker);
+        }
+        
+        /*lauftext4.fillText("", 3480, 1000);*/
+    }
+
+    
 
     //var wasserzeichen = c.getContext("2d");
     //wasserzeichen.font = "150px Anek Odia";
